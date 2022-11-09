028e97c511265c13ef38633309116d6ad5106223
lizbisschop <lizbisschop@hotmail.com>
2022-11-09 15:27:40 +0100

    better version

--- src/game.js
+++ src/game.js
@@ -1,11 +1,26 @@
 import { words } from "./words";
 import "./styles.css";
 
+//Running the game on your own machine
+//brew install nvm
+// mkdir ~/.nvm
+// add to ~/.zshrc:
+//  export NVM_DIR="$HOME/.nvm"
+// [ -s "/Users/lbisscho/.brew/opt/nvm/nvm.sh" ] && \. "/Users/lbisscho/.brew/opt/nvm/nvm.sh"  # This loads nvm
+// [ -s "/Users/lbisscho/.brew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/Users/lbisscho/.brew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+// nvm install v12.22.9
+// npm install
+// npm audit fix
+// npm run start
+
 const answer = "codam";
 
 export const validateGuess = (guess) => {
   console.clear();
-  if (guess.length < 5) {
+  if (!guess) {
+    console.log("Guess cannot be empty");
+    return;
+  } else if (guess.length < 5) {
     console.log("Not enough letters!");
     return;
   } else if (guess.length > 5) {
@@ -23,14 +38,13 @@ export const validateGuess = (guess) => {
     }
   }
   for (let i = 0; i < guess.length; i++) {
-    if (
-      answer.search(guess[i]) === guess.search(guess[i]) &&
-      i === answer.search(guess[i])
-    ) {
+    if (guess[i] === answer[i]) {
       console.log("[" + guess[i] + "] is in the right spot");
-    } else if (answer.search(guess[i]) > -1) {
+    } else if (answer.includes(guess[i])) {
       console.log("[" + guess[i] + "] exists but is not in the right spot");
-    } else if (answer.search(guess[i]) === -1)
+    } else {
       console.log("[" + guess[i] + "] is not in this word");
+    }
   }
 };
+
